266eb7b96f019c2cacc69dae5b6cadf2796cc29e
Merlin <146963793+miftane@users.noreply.github.com>
2026-04-05 17:39:50 +0300

    Create lab7.1.py

---
+++ lab7.1.py
@@ -0,0 +1,11 @@
+# 7.1.py
+numbers = [3, 7, 12, 5, 9]
+user_number = int(input("Введите число: "))
+
+print(f"Исходный список: {numbers}")
+print(f"Ваше число: {user_number}")
+
+if user_number in numbers:
+    print("Поздравляю, Вы угадали число!")
+else:
+    print("Нет такого числа!")
